7ef478e920f4de25fd6a9f8d2104095f13bf3097
jonni1x <jonzylfiu5@gmail.com>
2023-02-22 13:56:33 -0800

    commit

---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomeAnimation from '../components/HomeAnimation'
+
+const Home = () => {
+  return (
+    <div>
+        <HomeAnimation />
+    </div>
+  )
+}
+
+export default Home
