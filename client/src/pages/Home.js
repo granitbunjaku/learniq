3c06f4a647e0f30da9e42b9dbceb967b481c45c3
jonni1x <jonzylfiu5@gmail.com>
2023-02-23 05:24:45 -1100

    12

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
               <h3 className='font-medium'>The Ultimate Learning Platform for Professionals and Lifelong Learners</h3>
               <p className='my-8'>Our platform is designed to make learning easy, convenient, and fun. With intuitive navigation and personalized recommendations, you can quickly find the courses that are right for you and track your progress as you learn</p>
               <div className='flex justify-between w-[25rem]'>
-                <button class="bg-[#06a94d] hover:bg-[#059142]vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv text-white font-bold py-3 px-4 rounded">
+                <button class="bg-[#06a94d] hover:bg-[#059142] text-white font-bold py-3 px-4 rounded">
                   Become A Teacher
                 </button>
                 <button class="border-blue-600 border-2 hover:bg-blue-800 hover:text-white text-blue-600 font-bold py-3 px-4 rounded">
