7199dbe8353c861f741c03ae7e854247b04a72a8
jonni1x <jonzylfiu5@gmail.com>
2023-02-24 06:32:50 -1100

    .

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import HomeAnimation from '../components/HomeAnimation'
+import ClassCard from '../components/ClassCard';
+import Swiper from '../components/Swiper';
 
 const Home = () => {
   return (
-    <div>
-        <div className='flex h-80 items-center mt-24'>
+    <main>
+        <section className="flex lg:flex-row xs:flex-col xs:items-center lg:mt-16 w-full justify-center xs:gap-0 xs:text-center gap-16">
             <HomeAnimation />
             <div className='font-poppins w-1/2 flex flex-col items-center'>
               <h1 className='font-bold mt-12 text-2xl text-center'>LearnIQ - Master New Skills with Ease</h1>
               <h3 className='font-medium'>The Ultimate Learning Platform for Professionals and Lifelong Learners</h3>
               <p className='my-8'>Our platform is designed to make learning easy, convenient, and fun. With intuitive navigation and personalized recommendations, you can quickly find the courses that are right for you and track your progress as you learn</p>
-              <div className='flex justify-between w-[25rem]'>
+              <div className='flex justify-center gap-4'>
                 <button class="bg-[#06a94d] hover:bg-[#059142] text-white font-bold py-3 px-4 rounded">
                   Become A Teacher
                 </button>
@@ -18,10 +20,14 @@ const Home = () => {
                   Become A Learner
                 </button>
               </div>
-              
             </div>
-        </div>
-    </div>
+        </section>
+
+      <section className='py-24'>
+        <h3 className='font-poppins text-lg px-12'>Some Courses of different categories:</h3>
+        <Swiper />
+      </section>
+    </main>
   )
 }
 
