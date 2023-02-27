55ed27abd803472261755fc16f5e45f0306333d4
jonni1x <jonzylfiu5@gmail.com>
2023-02-27 03:51:22 -1100

    don't merge this

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,10 @@ import Home from './pages/Home';
 import Navbar from './components/Navbar'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
+import Profile from './pages/Profile'
+import { Teach } from './pages/Teach'
 import { Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -11,9 +14,11 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/become-mentor' element={<Teach />} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Signup />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,43 +1,65 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { googleLogout, useGoogleLogin } from '@react-oauth/google';
+import GoogleButton from 'react-google-button'
+import axios from 'axios';
 
 const Login = () => {
+    const [ user, setUser ] = useState(null);
+    const [ profile, setProfile ] = useState([]);
+
+    const login = useGoogleLogin({
+        onSuccess: (codeResponse) => setUser(codeResponse),
+        onError: (error) => console.log('Login Failed:', error)
+    });
+
+
+    useEffect(() => {
+        if (user) {
+            axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user?.access_token}`, {
+                    headers: {
+                        Authorization: `Bearer ${user.access_token}`,
+                        Accept: 'application/json'
+                    }
+                })
+                .then((res) => {
+                    setProfile(res.data);
+                    console.log(res.data)
+                })
+                .catch((err) => console.log(err));
+        } 
+    }, [user])
+
+
     return (
-        <section class="bg-gray-900">
-            <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-                    <img class="w-54 h-12 mr-2" src={require('../assets/images/logo.png')} alt="logo"/>
-                </a>
-                <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                    <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                        <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                            Create and account
-                        </h1>
-                        <form class="space-y-4 md:space-y-6" action="#">
-                            <div>
-                                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
-                            </div>
-                            <div>
-                                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
-                            </div>
-                            <div class="flex items-start">
-                                <div class="flex items-center h-5">
-                                    <input id="terms" aria-describedby="terms" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required=""/>
-                                </div>
-                                <div class="ml-3 text-sm">
-                                    <label for="terms" class="font-bold text-gray-500 dark:text-gray-300">Remember me</label>
-                                </div>
-                            </div>
-                            <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
-                            <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                                Don't have an account yet? <Link to="/signup" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Signup here</Link>
-                            </p>
-                        </form>
+        <section className='w-screen h-[80vh] flex flex-col justify-center'>
+            <h3 className='text-center my-12 font-semibold text-2xl font-poppins'>Log In To Your Account</h3>
+            <div className='flex items-center flex-col'>
+                <form className='flex flex-col xl:w-1/3 lg:w-1/2 md:w-2/4 xs:w-2/3 gap-8'>
+                    <div className='mx-auto'>
+                        <GoogleButton
+                            label='Log In With Google'
+                            onClick={() => login()}
+                        />
                     </div>
+                    <div className="relative">
+                        <input autoComplete='off' type="text" id="email" className="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                        <label htmlFor="email" className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">Email</label>
+                    </div>
+                    <div className="relative">
+                        <input autoComplete="off" type="password" id="password" className="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                        <label htmlFor="password" className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">Password</label>
+                    </div>
+                    <button className="bg-blue-600 text-white font-semibold py-2 w-full rounded">
+                        Login
+                    </button>
+                </form>
+                <div>
+                    <span className='pr-2'>Don't have an account?</span> 
+                    <Link to="/register" className='text-indigo-800 font-bold font-poppins'>Sign up</Link>
                 </div>
             </div>
+            
         </section>
     )
 }
