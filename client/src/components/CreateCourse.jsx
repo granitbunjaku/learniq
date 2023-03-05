391fb49f258f8dfd2e6975c30e26c3cbae9d7230
granitbunjaku <granitbunjaku14@gmail.com>
2023-03-05 13:43:38 +0100

    finished createcourse

--- client/src/components/CreateCourse.jsx
+++ client/src/components/CreateCourse.jsx
@@ -8,7 +8,6 @@ const CreateCourse = () => {
       endDate: new Date().setMonth(11)
     });
     const handleValueChange = (newValue) => {
-      console.log("newValue:", newValue);
       setValue(newValue);
     }
   
@@ -18,35 +17,35 @@ const CreateCourse = () => {
 
         <form class="p-6">
             <div class="mb-6">
-                <label for="email" class="mb-2 block text-sm font-medium text-gray-900">Course Name</label>
-                <input type="email" id="email" class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 shadow-sm focus:border-blue-500 focus:ring-blue-500" placeholder="name@flowbite.com" required />
+                <label for="email" class="mb-2 block text-sm font-medium font-poppins text-gray-900">Course Name</label>
+                <input type="email" id="email" class="block w-full  font-poppins rounded-lg border border-gray-300 bg-white p-2.5 text-sm text-gray-900 shadow-sm focus:border-blue-500 focus:ring-blue-500" placeholder="name@flowbite.com" required />
                 </div>
                 <div class="mb-6">
-                <label for="price" class="mb-2 block text-sm font-medium text-gray-900">Course Price</label>
-                <input type="number" id="price" class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 shadow-sm focus:border-blue-500 focus:ring-blue-500" required />
+                <label for="price" class="mb-2 block text-sm font-medium font-poppins text-gray-900">Course Price</label>
+                <input type="number" id="price" class="block w-full  font-poppins rounded-lg border border-gray-300 bg-white p-2.5 text-sm text-gray-900 shadow-sm focus:border-blue-500 focus:ring-blue-500" required />
                 </div>
                 <div class="relative mb-6">
-                    <label for="price" class="mb-2 block text-sm font-medium text-gray-900">Last Enroll date</label>
+                    <label for="price" class="mb-2 block text-sm font-medium font-poppins text-gray-900">Last Enroll date</label>
                     <Datepicker
-                        inputClassName="font-poppins bg-white dark:bg-white dark:placeholder:text-white"
+                        inputClassName="font-poppins bg-white dark:bg-white dark:placeholder:text-white border border-gray-300"
                         value={value}
                         onChange={handleValueChange}
                     />
                 </div>
 
                 <div class="mb-6">
-                    <label for="message" class="mb-2 block text-sm font-medium text-gray-900">Describe the course</label>
-                    <textarea id="message" rows="4" class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500" placeholder="Leave a comment..."></textarea>
+                    <label for="message" class="mb-2 block text-sm font-medium font-poppins text-gray-900">Describe the course</label>
+                    <textarea id="message" rows="4" class="block w-full rounded-lg border border-gray-300 bg-white p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500" placeholder="Leave a comment..."></textarea>
                 </div>
 
                 <div class="mb-6 flex items-start">
                     <div class="flex h-5 items-center">
-                        <input id="terms" type="checkbox" value="" class="focus:ring-3 h-4 w-4 rounded border border-gray-300 bg-gray-50 focus:ring-blue-300" required />
+                        <input id="terms" type="checkbox" value="" class="focus:ring-3 h-4 w-4 font-poppins rounded border border-gray-300 bg-gray-50 focus:ring-blue-300" required />
                     </div>
-                    <label for="terms" class="ml-2 text-sm font-medium text-gray-900">I agree with the <a href="#" class="text-blue-600 hover:underline">terms and conditions</a></label>
+                    <label for="terms" class="ml-2 text-sm font-medium text-gray-900 font-poppins">I agree with the <a href="#" class="text-blue-600 hover:underline">terms and conditions</a></label>
                 </div>
 
-                <button type="submit" class="rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300">Register new account</button>
+                <button type="submit" class="rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-poppins font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300">Create new course</button>
 
             </form>
         </div>
